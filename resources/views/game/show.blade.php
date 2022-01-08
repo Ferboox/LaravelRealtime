24b32901c71728b3cfc812eb2589be3fce84012f
Ferboox <ferbargar99@hotmail.com>
2022-01-08 15:42:22 -0600

    22.- Juego de ruleta

---
+++ resources/views/game/show.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@push('styles')
+    <style type="text/css">
+        @keyframes rotate{
+            from{
+                transform: rotate(0deg);
+            }
+            to{
+                transform: rotate(360deg);
+            }
+        }
+
+        .refresh{
+            animation: rotate 1.5s linear infinite;
+        }
+    </style>
+@endpush
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Game</div>
+
+                <div class="card-body">
+                    <div class="text-center">
+                        <img id="circle" class="refresh" width="250" height="250" src="{{ asset('img/circle.png') }}" alt="">
+                        <p id="winner" class="display-1 d-none text-primary">10</p>
+                    </div>
+                    <hr>
+                    <div class="text-center">
+                        <label class="font-weight-bold h5">Your bet</label>
+                        <select id="bet" class="custom-select">
+                            <option selected>Not in</option>
+                            @foreach (range(1,12) as $number)
+                                <option>{{ $number }}</option>
+                            @endforeach
+                        </select>
+                        <hr>
+                        <p class="font-weight-bold h5">Remaining time</p>
+                        <p id="timer" class="h5 text-danger">Waiting start</p>
+                        <hr>
+                        <p id="result" class="h1"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
